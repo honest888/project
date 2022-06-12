4212d15139e121acb7e2d369b1962a68530d8f5c
dufgu888 <dufgu888@gmail.com>
2022-06-11 17:23:12 -1200

    Revert "Merge branch 'development' of https://github.com/bankmil/ControlPanel_lite into development"
    This reverts commit 4b9fc9d9d81098bf8637ff9426b9a6bad762e43b, reversing
    changes made to 6278b96b77f55401d64f8470a8a8dcd517f69223.

--- savvyvan/proj/webapp/jsonFileReader.py
+++ savvyvan/proj/webapp/jsonFileReader.py
@@ -75,8 +75,8 @@ class ConfigFileReader():
         self.file_path =  None
         path_options = [
             # os.path.join(os.getcwd(), 'config.json') ,
-            os.path.join('home/pi/savvyvan', 'proj','webapp', 'config.json') ,
-            os.path.join('home/pi/savvyvan', 'proj','webapp', 'config.json') , 
+            os.path.join('/home/pi/savvyvan', 'proj','webapp', 'config.json') ,
+            os.path.join('/home/pi/savvyvan', 'proj','webapp', 'config.json') , 
             os.path.join(os.getcwd(), 'webapp','config.json') ,
             os.path.join(os.getcwd(), 'config.json') ,
         ]
